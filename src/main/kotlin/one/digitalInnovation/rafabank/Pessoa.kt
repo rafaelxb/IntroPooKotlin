87bb9941c41302122632a21595c79e155bf1acdc
rafaelxb <rfxbarbosa@gmail.com>
2022-06-05 17:21:23 -0300

    Adição Inner Class

--- src/main/kotlin/one/digitalInnovation/rafabank/Pessoa.kt
+++ src/main/kotlin/one/digitalInnovation/rafabank/Pessoa.kt
@@ -3,6 +3,10 @@ package one.digitalInnovation.rafabank
 class Pessoa {
     var nome: String = "Teste"
     var cpf: String = "123.456.789-01"
+
+    inner class Endereco {
+        var rua: String = "Rua Teste"
+    }
 }
 
 fun main(){
