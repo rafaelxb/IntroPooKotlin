e996e2685021d22b497471430dd77d17a0de8e18
rafaelxb <rfxbarbosa@gmail.com>
2022-06-19 10:15:12 -0300

    crianda class conta

---
+++ src/main/kotlin/one/digitalInnovation/rafabank/Conta.kt
@@ -0,0 +1,17 @@
+package one.digitalInnovation.rafabank
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String,
+    val conta: String,
+    val saldo: BigDecimal
+) {
+    fun deposito(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+
+    }
+}
